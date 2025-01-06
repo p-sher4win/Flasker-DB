327238a4028169f6f813478f45717049c91b4e7c
p-sher4win <sher4winpereira@gmail.com>
2025-01-06 19:31:15 +0530

    new updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -72,6 +72,7 @@ def name():
                            name=name,
                            form=form)
 
+# Add Record to MYSQL Database
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
     name = None
@@ -94,6 +95,34 @@ def add_user():
                            name=name,
                            form=form,
                            our_users=our_users)
+
+# Update MYSQL Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash("User Updated Successful")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there was a problem... try again!!!")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+    else:
+        return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+        
+
+
+
 
 
 if __name__ == "__main__":
